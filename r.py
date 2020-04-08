c26cb65ed383a9960684487ebf5e6a99cbbe9d7b
Chen Jui <larstraschen@gmail.com>
2020-04-08 17:03:05 +0800

    count

--- r.py
+++ r.py
@@ -1,12 +1,15 @@
 import random
 r = random.randint(1,100)
+count = 0
 while True:
+	count += 1
 	num = input('請輸入數字: ')
 	num = int(num)
 	if r == num:
-		print('於猜對了')
+		print('猜對了')
 		break
 	elif num > r:
 		print('比較大喔')
 	else:
 		print('比較小喔')
+	print('這是你猜的第', count, '次')
